e264ae38e3341cd828b71cf9ab727744834f9025
claudia <claudiacrpsantos@hotmail.com>
2019-03-03 00:46:44 +0100

    weather images added

--- scripts/weather.js
+++ scripts/weather.js
@@ -20,7 +20,32 @@ function weather(data) {
     var cityName = document.getElementById("cityName");
     var temperature = document.getElementById("temperature");
     var description = document.getElementById("description");
+    var weatherImage = document.getElementById("weatherImage");
     cityName.append(data.name + ", " + data.sys.country);
-    temperature.append((data.main.temp).toFixed(0));
+    temperature.append((data.main.temp).toFixed(0) + "°C");
     description.append(data.weather[0].description);
+    if((data.weather[0].description == "scattered clouds") || (data.weather[0].description == "broken clouds") ){
+        var img = document.createElement("img");
+        img.className = "scattered_clouds";
+        img.src = "styles/scattered_clouds.png";
+        weatherImage.append(img);
+    }
+    if(data.weather[0].description == "clear sky"){
+        var img = document.createElement("img");
+        img.className = "clear_sky";
+        img.src = "styles/clear_sky.png";
+        weatherImage.append(img);
+    }
+    if(data.weather[0].description == "few clouds"){
+        var img = document.createElement("img");
+        img.className = "few_clouds";
+        img.src = "styles/few_clouds.png";
+        weatherImage.append(img);
+    }
+    if(data.weather[0].description == "snow" || data.weather[0].description == "light snow"){
+        var img = document.createElement("img");
+        img.className = "snow";
+        img.src = "styles/snow.png";
+        weatherImage.append(img);
+    }
 }
