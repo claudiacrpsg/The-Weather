a53cd536996e5d395f49e9e10ec28f38228e3f7e
claudia <claudiacrpsantos@hotmail.com>
2019-03-18 18:03:32 +0100

    js function updated and completed

--- scripts/weather.js
+++ scripts/weather.js
@@ -10,7 +10,7 @@ function search() {
             data = json;
             console.log(data);
             weather(data);
-           
+
         })
         .catch((err) => {
             console.log(err);
@@ -18,30 +18,25 @@ function search() {
 }
 
 function weather(data) {
+    document.getElementById("userInput").value = "";
+    document.getElementById("weatherInfo").innerHTML = "";
+   
+    var weatherInfo = document.getElementById("weatherInfo");
+    var cityName = document.createElement("h5");
+    var temperature = document.createElement("h5");
+    var maxTemperature = document.createElement("h5");
+    var minTemperature = document.createElement("h5");
+    var description = document.createElement("h5");
 
-    var weatherTiles = document.getElementById("weatherTiles");
-
-    var cityName = document.getElementById("cityName");
-    var temperature = document.getElementById("temperature");
-    var maxTemperature = document.getElementById("maxTemperature");
-    var minTemperature = document.getElementById("minTemperature");
-    var description = document.getElementById("description");
     if (data.cod == "404") {
-      
-        cityName.style.display = "none";
-        temperature.style.display = "none";
-        maxTemperature.style.display = "none";
-        minTemperature.style.display = "none";
-        description.style.display = "none";
-        document.getElementById("cityNotFound").style.display = "block";
+        cityName.append("No results matched your search! Please try again.");
+        weatherInfo.append(cityName);
     } else {
-        
         cityName.append("City: " + data.name + ", " + data.sys.country);
         temperature.append("Current temperature: " + (data.main.temp).toFixed(0) + "°C");
         maxTemperature.append("Max temperature: " + (data.main.temp_max).toFixed(0) + "°C");
         minTemperature.append("Min temperature: " + (data.main.temp_min).toFixed(0) + "°C");
         description.append("Weather conditions: " + data.weather[0].description);
-        weatherTiles.append(cityName, temperature, maxTemperature, minTemperature, description);
+        weatherInfo.append(cityName, temperature, maxTemperature, minTemperature, description);
     }
-    
 }
